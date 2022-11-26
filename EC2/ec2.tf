f68fb25b784f81c3db16317c4522aa6aecf63f80
Reshma <pathipati.reshma@gmail.com>
2022-11-26 12:02:38 +0530

    trash commit

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -7,6 +7,12 @@ resource "aws_instance" "this" {
     hibernation                 =    var.hibernation
     instance_type               =    var.instance_type
     key_name                    =    var.key_name
+    disable_api_termination     =    var.disable_api_termination
+    user_data                   =    var.user_data
+    user_data_base64            =    var.user_data_base64
+    user_data_replace_on_change =    var.user_data_replace_on_change
+    subnet_id                   =    var.subnet_id
+    vpc_security_group_ids      =    var.vpc_security_group_ids
     dynamic "launch_template" {
     for_each = var.launch_template != null ? [var.launch_template] : []
     content {
@@ -15,4 +21,20 @@ resource "aws_instance" "this" {
       version = lookup(var.launch_template, "version", null)
       }
     }
+    dynamic "ebs_block_device" {
+    for_each = var.ebs_block_device
+    content {
+      delete_on_termination = lookup(ebs_block_device.value, "delete_on_termination", null)
+      device_name           = ebs_block_device.value.device_name
+      encrypted             = lookup(ebs_block_device.value, "encrypted", null)
+      iops                  = lookup(ebs_block_device.value, "iops", null)
+      kms_key_id            = lookup(ebs_block_device.value, "kms_key_id", null)
+      snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
+      volume_size           = lookup(ebs_block_device.value, "volume_size", null)
+      volume_type           = lookup(ebs_block_device.value, "volume_type", null)
+      throughput            = lookup(ebs_block_device.value, "throughput", null)
+    }
+  }
+    
+
 }
--- EC2/vars.tf
+++ EC2/vars.tf
@@ -34,3 +34,32 @@ variable "launch_template" {
     type = map(string)
   
 }
+variable "disable_api_termination" {
+    type        = bool
+  
+}
+variable "ebs_block_device" {
+    type        = list(map(string))
+    default     = []
+}
+variable "user_data" {
+    type = string
+    default = "null"
+}
+variable "user_data_base64" {
+    type = string
+    default = "null"
+}
+variable "user_data_replace_on_change" {
+    type = string
+    default = "null"
+}
+variable "subnet_id" {
+    type        = string
+    default     = null
+}
+variable "vpc_security_group_ids" {
+    type        = list(string)
+    default     = null
+}
+  
